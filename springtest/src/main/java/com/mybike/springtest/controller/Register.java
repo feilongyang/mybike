43ffa582432af5fb8b88b164462fe8ed3e057f2d
feilongyang <fealenyoung@qq.com>
2015-06-17 11:14:01 +0800

    commit by yangfeilong

--- springtest/src/main/java/com/mybike/springtest/controller/Register.java
+++ springtest/src/main/java/com/mybike/springtest/controller/Register.java
@@ -3,5 +3,6 @@ package com.mybike.springtest.controller;
 public class Register {
     public static void main(String[] args) {
         System.out.println("from mac...");
+        System.out.println("from pc...");
     }
 }
